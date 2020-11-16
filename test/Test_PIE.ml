ec807d22ed40ebee5a28d7a3a78f511db83f77e2
Saswat Padhi <saswat.sourav@gmail.com>
2020-11-15 23:50:48 -0500

    added tests from LoopInvGen & fixed a major bug

---
+++ test/Test_PIE.ml
@@ -0,0 +1,79 @@
+open Base
+
+open LoopInvGen
+
+open PIE
+open Utils
+
+let abs_job = Job.create ()
+  ~args:([ "x", Type.INT ])
+  ~features:[
+    ((fun [@warning "-8"] [Value.Int x] -> x > 0), "(> x 0)")
+  ]
+  ~pos_tests:(List.map [3 ; 0 ; 6] ~f:(fun i -> [Value.Int i]))
+  ~neg_tests:(List.map [(-1) ; (-2)] ~f:(fun i -> [Value.Int i]))
+
+let conflicts_to_string cgroup =
+  let tests_to_string vs = "[" ^ (List.to_string_map ~sep:"," ~f:Value.to_string vs) ^ "]"
+  in "Pos:{"
+   ^ List.(to_string_map (sort cgroup.pos ~compare:Poly.compare)
+                         ~sep:" ; " ~f:tests_to_string)
+   ^ "} + Neg:{"
+   ^ List.(to_string_map (sort cgroup.neg ~compare:Poly.compare)
+                         ~sep:" ; " ~f:tests_to_string)
+   ^ "}"
+
+let abs_conflict_failure () =
+  let (res, _) = learnPreCond abs_job ~config:{ PIE.Config.default with
+                                                disable_synth = true }
+  in Alcotest.(check string) "identical" "false" (cnf_opt_to_desc res)
+
+let abs_conflict_group () =
+  let res = List.to_string_map (conflictingTests abs_job)
+                               ~sep:"\n" ~f:conflicts_to_string
+  in Alcotest.(check string) "identical" "Pos:{[0]} + Neg:{[-2] ; [-1]}" res
+
+let abs_precond_1_cnf () =
+  let (res, _) = learnPreCond (Job.add_feature ~job:abs_job
+                                 ((fun [@warning "-8"] [Value.Int x] -> x + x = x),
+                                 "(= x (+ x x))"))
+                              ~config:{ PIE.Config.default with
+                                        _BFL = {
+                                          BFL.Config.default with
+                                          min_clauses = 1
+                                        ; auto_increment_clauses = false
+                                        }
+                                      ; disable_synth = true
+                                      }
+  in Alcotest.(check string) "identical" "false" (cnf_opt_to_desc res)
+
+let abs_precond_auto_1 () =
+  let (res, _) = learnPreCond (Job.add_feature ~job:abs_job
+                                 ((fun [@warning "-8"] [Value.Int x] -> x + x = x),
+                                 "(= x (+ x x))"))
+                               ~config:{ PIE.Config.default with
+                                        _BFL = {
+                                          BFL.Config.default with
+                                          min_clauses = 1
+                                        ; auto_increment_clauses = true
+                                        }
+                                      ; disable_synth = true
+                                      }
+  in Alcotest.(check string) "identical" "(or (= x (+ x x)) (> x 0))"
+                             (cnf_opt_to_desc res)
+
+let abs_zero_initial_features () =
+  let (res, _) = learnPreCond (Job.create ()
+    ~args:([ "x", Type.INT ])
+    ~features:[]
+    ~pos_tests:(List.map [3 ; 0 ; 6] ~f:(fun i -> [Value.Int i]))
+    ~neg_tests:(List.map [(-1) ; (-2)] ~f:(fun i -> [Value.Int i])))
+  in Alcotest.(check string) "identical" "(>= x 0)" (cnf_opt_to_desc res)
+
+let all = [
+  "Abs Conflict Failure",    `Quick, abs_conflict_failure  ;
+  "Abs Conflict Group",      `Quick, abs_conflict_group    ;
+  "Abs Precond 1-CNF",       `Quick, abs_precond_1_cnf ;
+  "Abs Precond Auto >= 1",   `Quick, abs_precond_auto_1 ;
+  "Abs No Initial Features", `Quick, abs_zero_initial_features ;
+]
